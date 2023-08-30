731c5c4603863370a54ae8712faa85fca0e567c8
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-30 08:59:41 -0300

    login casi

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from '/src/components/login.js'
+import Login from './src/components/login.js'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>TP Comidas</Text>
       <Login></Login>
       <StatusBar style="auto" />
     </View>
@@ -19,4 +19,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize: 30,
+    fontWeight: 'bold',
+    paddingBottom: 10
+  }
 });
--- src/components/login.js
+++ src/components/login.js
@@ -1,25 +1,70 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View, TextInput } from "react-native";
+import { StyleSheet, View, TextInput, TouchableOpacity, Text} from "react-native";
+import { Formik } from "formik";
 
-const login = () => {
+const Login = () => {
+
+  
+
   return (
-    <View>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={username}
-      />
-    </View>
+    <Formik initialValues={{ username: '', password: ''}} onSubmit={(values) => console.log(values)}>
+      {({ handleSubmit, handleChange, handleBlur, values  }) => (
+        <View>
+          <TextInput
+            style={styles.input}
+            value={values.username}
+            onChangeText={handleChange('username')}
+            onBlur={handleBlur('username')}
+            placeholder="Username"
+
+
+          />
+          <TextInput
+            secureTextEntry={true}
+            style={styles.input}
+            value={values.password}
+            onChangeText={handleChange('password')}
+            onBlur={handleBlur('password')}
+            placeholder="Password"
+
+
+          />
+          <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+          <Text>Ingresar</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </Formik>
   );
 };
 
 const styles = StyleSheet.create({
-    input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
+  input: {
+    padding: 15,
+    width: 330,
+    borderRadius: 20,
+    shadowOffset: {
+      width: 3,
+      height: 5,
     },
-  });
-  
-  export default TextInputExample;
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    marginBottom: 15,
+    placeholderTextColor: "gray",
+  },
+  button: {
+    alignItems: "center",
+    padding: 15,
+    paddingHorizontal: 80,
+    borderRadius: 20,
+
+    shadowOffset: {
+      width: 3,
+      height: 5,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+  },
+});
+
+export default Login;
